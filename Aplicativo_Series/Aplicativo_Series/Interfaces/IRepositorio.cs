afdc38c365bf6fe03aaa29074618f7fc1af09dea
delclecianojunior <delclecianobj@hotmail.com>
2022-02-16 22:32:00 -0300

    Adicionando o codigo de csharp relativo ao cadastro de series

---
+++ Aplicativo_Series/Aplicativo_Series/Interfaces/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aplicativo_Series
+{
+    public interface IRepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(Serie objeto);
+        void Exclui(int id);
+        void Atualiza(int id, Serie objeto);
+        int ProximoId();
+    }
+}
